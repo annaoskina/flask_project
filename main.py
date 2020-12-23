03f5460ea499db18796a8d5eb3c112d070956e8c
annaoskina <annaoskina@gmail.com>
2020-12-23 20:37:24 +0300

    made backend

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import csv
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
 
-@app.route('/home')
+@app.route('/home.html')
 def home():
     return render_template('home.html')
 
-@app.route('/about')
+@app.route('/index_about.html')
 def index_about():
     return render_template('index_about.html')
 
-@app.route('/research')
+@app.route('/index_research.html')
 def index_research():
     return render_template('index_research.html')
 
-@app.route('/data')
+@app.route('/index_data.html')
 def index_data():
     return render_template('index_data.html')
 
-
-def get_search_results(q):
-    if q == "Akutagawa":
-        result = ["Rashomon", "Majutsu"]
-    else:
-        result = [""]
-    return result
-
-@app.route('/search', methods=["get", "post"])
-def search():
-    q=request.values.get("q", "")
-    print(q)
-    result = get_search_results(q)
-    return render_template('search.html', q=q, result=result)
+@app.route('/writings')
+def get_writings():
+    filterStr = request.args.get("filter", "")
+    new_dic = []
+    with open('Correlation.csv') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            if filterStr.lower() in row['Author'].lower():
+                new_dic.append(row)
+    return jsonify(new_dic)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
 
 
 
